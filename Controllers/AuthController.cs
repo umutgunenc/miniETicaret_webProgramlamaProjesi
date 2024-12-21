f419261a3063f7bdced3ec9ea52b5814585a307b
umutgunenc <umutgunenc@gmail.com>
2024-12-21 23:45:53 +0300

    Viewler için taslaklar hazırlanmaya başlandı

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace miniETicaret.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
