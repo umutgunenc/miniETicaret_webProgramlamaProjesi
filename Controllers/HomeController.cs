49170ef61ca8cae9325ad0f65e5848bc06d747a9
umutgunenc <umutgunenc@gmail.com>
2024-12-22 18:28:58 +0300

    Ana Sayfada Ürün Listeleme Yapıldı

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using miniETicaret.Data;
+using miniETicaret.Models.ViewModel.Home;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace miniETicaret.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly eTicaretDBContext _eTicaretDBContext;
+
+        public HomeController(eTicaretDBContext eTicaretDBContext)
         {
-            return View();
+            _eTicaretDBContext = eTicaretDBContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel model = new(_eTicaretDBContext);
+            model.Products = await model.GetProductsAsync();
+            return View(model);
         }
     }
 }
---
+++ Models/ViewModel/Home/IndexViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using miniETicaret.Data;
+using miniETicaret.Models.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace miniETicaret.Models.ViewModel.Home
+{
+    public class IndexViewModel
+    {
+        private readonly eTicaretDBContext _eTicaretDBContext;
+
+        public IndexViewModel()
+        {
+
+        }
+
+        public IndexViewModel(eTicaretDBContext eTicaretDBContext)
+        {
+            _eTicaretDBContext = eTicaretDBContext;
+        }
+
+
+        public List<Product> Products { get; set; }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            IQueryable<Product> query = _eTicaretDBContext.Products
+                .Where(x => x.IsActive == true)
+                .Select(x => new Product
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Despriction = x.Despriction,
+                    ImgUrl = x.ImgUrl
+                });
+
+            Products = await query.ToListAsync();
+            return Products;
+        }
+    }
+}
