544c74018ab8d30cb1958cb560cd72645d639ea4
umutgunenc <umutgunenc@gmail.com>
2024-12-27 18:28:51 +0300

    commit

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -140,7 +140,7 @@ namespace miniETicaret.Controllers
                 CustomerId = customer.Id,
                 OrderTime = DateTime.Now,
                 TotalPrice = totalPrice,
-                Despriction = $"{customer.Name} {customer.SurName} -- {DateTime.Now.ToString("dd:MM:yyyy")}"
+                Despriction = $"{customer.Name} {customer.SurName} -- {DateTime.Now.ToString("mm:HH dd:MM:yyyy")}"
             };
 
             await _eTicaretDBContext.Orders.AddAsync(order);
